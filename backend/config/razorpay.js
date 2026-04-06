567ed7f0b4dca6aecb0304b4bc8bd68e01afe943
ankitsunil530 <22bcs244@iiitdmj.ac.in>
2026-04-06 23:06:55 +0530

    strike 2

--- backend/config/razorpay.js
+++ backend/config/razorpay.js
@@ -1,7 +1,7 @@
 import Razorpay from "razorpay";
 import dotenv from "dotenv";
 dotenv.config();
-
+// razor pay integration
 const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_KEY_SECRET,
